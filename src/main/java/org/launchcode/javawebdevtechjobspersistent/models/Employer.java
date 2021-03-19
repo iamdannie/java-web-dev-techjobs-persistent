f125bd9c01ee854e9f336bc04676803a90b3db51
iamdannie <iamtaiyco@gmail.com>
2021-03-18 22:07:26 -0500

    updated up to the Controllers

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,22 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @Id @GeneratedValue
+        private String location;
+    public Employer () {
 
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
